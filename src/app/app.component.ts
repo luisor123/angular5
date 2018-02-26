ac0b4494fe08f32c49f5daedc1cb5e5e43f1d26b
luisor <luisorstaby@gmail.com>
2018-02-26 11:59:09 -0500

    Se agregaron los componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
+  selector: 'app-root1',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  public var1 = 'Bienvenido a las Ciudades calurosas';
+  public var2 = 15;
+  public hoy = new Date();
+  public nombre = 'Luis';
+
+  Verfechas() {
+    var dia;
+    var mes;
+    var anio;
+    dia = this.hoy.getDate();
+    mes = this.hoy.getMonth();
+    anio = this.hoy.getFullYear();
+    console.log(this.hoy);
+
+  }
 }
